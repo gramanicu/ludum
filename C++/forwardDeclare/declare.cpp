22a43db76d2300d65092a66eb49572af562f43c6
Grama Nicolae <nicolae.grama@stud.acs.upb.ro>
2019-03-01 21:09:21 +0200

    Added forwardDeclare

--- C++/forwardDeclare/declare.cpp
+++ C++/forwardDeclare/declare.cpp
@@ -1,1 +1,39 @@
 #include <iostream>
+
+int sum(int ,int);
+int difference(int, int);
+int product(int, int);
+int division(int, int);
+
+int main() {
+    int a, b;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+
+    std::cout << "Sum = " << sum(a, b) << std::endl;
+    std::cout << "Difference = " << difference(a, b) << "\n";
+    std::cout << "Product = " << product(a, b) << "\n";
+    std::cout << "Division (quotient?) = " << division(a, b) << "\n";
+
+    return 1;
+}
+
+int sum(int a, int b) {
+    return a+b;
+}
+
+int difference(int a, int b) {
+    return a-b;
+}
+
+int product(int a, int b) {
+    return a*b;
+}
+
+int division(int a, int b) {
+    return a/b;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Ludum
+
+The purpose of this project is to present different programming concepts through demos.
+
+Also, this repo is used to store different other material used for teaching.
+
+
+
